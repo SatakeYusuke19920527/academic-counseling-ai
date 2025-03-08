875c21771c65d7ec5cfdca88a6526578c2e665f7
mkayuri2009 <yuuy93543@gmail.com>
2025-03-08 21:08:03 +0900

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,40 +1,10 @@
-
 'use client'
+import Link from 'next/link';
+import { Box } from '@mui/material';
 
-import useCurrentUser from "@/hooks/useCurrentUser";
-import { loginRequest } from "@/lib/msalConfig";
-import { useMsal } from "@azure/msal-react";
-import { useEffect } from "react";
-import Link from "next/link";
-
-export default function Login() {
-    const { instance, accounts } = useMsal();
-    const user = useCurrentUser();
-
-    useEffect(() => {
-        console.log('🚀 ~ SideMenu ~ user:', user);
-        console.log('🚀 ~ SideMenu ~ accounts:', accounts);
-    }, [accounts]);
-    return (
-        <main>
-          <h1
-            onClick={() =>
-              user === null
-                ? instance?.loginRedirect?.(loginRequest)
-                : instance?.logout?.()
-            }
-            className="flex justify-center px-4 text-2xl font-bold cursor-pointer"
-          >
-            {user === null ? 'login' : 'logout'}
-          </h1>
-          <h1>
-            {user === null
-              ? 'Please login to see your profile'
-              : `Welcome ${user?.user_name}! 🚀 Warm Welcome 🚀 Your email: ${user?.email} && ${user?.sub}`}
-              <div>
-                <Link href="/">go to home</Link>
-                </div>
-          </h1>
-        </main>
-      );
-    }
+export default function Home() {
+return (
+  <Box sx={{ backgroundColor:"#",}}>
+  <Link href="/">go to home</Link>
+    </Box>
+);};
