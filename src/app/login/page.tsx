58946f7adb2d022676d604e379685b8965a0b527
mkayuri2009 <yuuy93543@gmail.com>
2025-03-29 20:43:04 +0900

    完了

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,12 @@
-//login/page.tsx
+// app/login/page.tsx
 'use client'
-import Link from 'next/link';
-import { Box } from '@mui/material';
+import Link from "next/link";
+import { Box } from "@mui/material";
 
 export default function Home() {
-return (
-  <Box sx={{ backgroundColor:"#",}}>
-  <Link href="/">go to home</Link>
+  return (
+    <Box sx={{ backgroundColor: "#f0f0f0" }}> {/* 背景色を正しい値に修正 */}
+      <Link href="/">Go to Home</Link>
     </Box>
-);};
+  );
+}
